f3596d989ab652a8662c3fa1a339296c611bbc79
Kevin Lu <lu3kevin@gmail.com>
2019-10-26 17:48:47 -0400

    new changes for #5

--- get-quote.py
+++ get-quote.py
@@ -9,7 +9,19 @@ def primary():
    
   last = len(quotes) - 1 
   rnd = random.randint(0, last)  
-  print(quotes[rnd])
+  print(quotes[rnd],end='', flush=True)
+  
+  #not the best implementation as it's repetitive but okay for start
+  last2 = len(quotes) - 1 
+  rnd2 = random.randint(0, last2) 
+  print(quotes[rnd2],end='', flush=True)
+  
+  #writing to file
+  file = open("quotes.txt", "a+")
+  text = input("user input: ")
+  file.write("\n" +text)
+  file.close()
+  
 
 if __name__== "__main__":
   primary()
